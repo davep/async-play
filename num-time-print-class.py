9166c391f110c3f9a34c20c9766af44c5fc5f9f7
Dave Pearson <davep@davep.org>
2022-11-08 15:28:55 +0000

    :sparkles: Update both time tests to do and result in the same thing

--- num-time-print-class.py
+++ num-time-print-class.py
@@ -2,23 +2,24 @@ import asyncio
 from random import randint
 from typing import Generator
 
-async def number( value: int ) -> None:
+async def number( value: int ) -> int:
     await asyncio.sleep( value )
     print( value )
+    return value
 
 class NumPrinter:
 
     def __init__( self, count: int ) -> None:
         self._count = count
 
-    def __await__( self ) -> Generator[ None, None, None ]:
-        async def num_printer() -> None:
-            await asyncio.gather( *(
+    def __await__( self ) -> Generator[ None, None, list[ int ] ]:
+        async def num_printer() -> list[ int ]:
+            return await asyncio.gather( *(
                 number( randint( 0, self._count ) ) for _ in range( self._count )
             ) )
         return num_printer().__await__()
 
 async def main():
-    await NumPrinter( 30 )
+    print( await NumPrinter( 30 ) )
 
 asyncio.run( main() )
--- num-time-print.py
+++ num-time-print.py
@@ -1,13 +1,14 @@
 import asyncio
 from random import randint
 
-async def number( value: int ) -> None:
+async def number( value: int ) -> int:
     await asyncio.sleep( value )
     print( value )
+    return value
 
-async def num_printer( count: int ) -> None:
-    await asyncio.gather( *(
+async def num_printer( count: int ) -> list[ int ]:
+    return await asyncio.gather( *(
         number( randint( 0, count ) ) for _ in range( count )
     ) )
 
-asyncio.run( num_printer( 30 ) )
+print( asyncio.run( num_printer( 30 ) ) )
